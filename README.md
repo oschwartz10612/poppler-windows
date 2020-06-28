a4f66fc054c68f4e8632dd3a513cb518f5d056ce
Owen Schwartz <oschwartz10612@gmail.com>
2020-06-28 14:39:49 -0400

    Build 0.89.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# Poppler Packaged for Windows
+
+The latest Poppler prebuilt-binaries packaged with dependencies. Built with the help of conda-forge.
+
+## Download
+
+You can download the latest build from [releases](https://github.com/oschwartz10612/poppler-windows/releases/).
+
+## Out of Date?
+
+Create a new pull request and bump POPPLER_VERSION to the latest. Make sure to tag the release incrementing it properly.
---
+++ package.sh
@@ -0,0 +1,18 @@
+mkdir "poppler-$POPPLER_VERSION"
+cd "poppler-$POPPLER_VERSION"
+
+cp -a $PKGS_PATH_DIR/poppler-$POPPLER_VERSION*/Library/. .
+cp $PKGS_PATH_DIR/freetype*/Library/bin/freetype.dll ./bin/
+cp $PKGS_PATH_DIR/zlib*/Library/bin/zlib.dll ./bin/
+cp -a $PKGS_PATH_DIR/zstd*/Library/bin/. ./bin/
+cp $PKGS_PATH_DIR/libtiff*/Library/bin/tiff.dll ./bin/
+cp $PKGS_PATH_DIR/libtiff*/Library/bin/libtiff.dll ./bin/
+cp $PKGS_PATH_DIR/libssh2*/Library/bin/libssh2.dll ./bin/
+cp $PKGS_PATH_DIR/libpng*/Library/bin/libpng16.dll ./bin/
+cp $PKGS_PATH_DIR/libcurl*/Library/bin/libcurl.dll ./bin/
+cp $PKGS_PATH_DIR/openssl*/Library/bin/libcrypto-1_1-x64.dll ./bin/
+cp $PKGS_PATH_DIR/openjpeg*/Library/bin/openjp2.dll ./bin/
+cp $PKGS_PATH_DIR/xz*/Library/bin/liblzma.dll ./bin/
+
+ls ./bin/
+
