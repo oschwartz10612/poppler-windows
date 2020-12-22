4ba5b853e0b9a9dda9b8ccef676023ab27470a40
Owen Schwartz <oschwartz10612@gmail.com>
2020-12-22 12:24:34 -0500

    Fix directory structure
    Poppler bin now in Library/bin/ and data in share/poppler/

--- package.sh
+++ package.sh
@@ -4,23 +4,22 @@ POPPLER_DATA_URL="https://poppler.freedesktop.org/poppler-data-0.4.10.tar.gz"
 mkdir "poppler-$POPPLER_VERSION"
 cd "poppler-$POPPLER_VERSION"
 
-cp -a $PKGS_PATH_DIR/poppler-$POPPLER_VERSION*/Library/. .
-cp $PKGS_PATH_DIR/freetype*/Library/bin/freetype.dll ./bin/
-cp $PKGS_PATH_DIR/zlib*/Library/bin/zlib.dll ./bin/
-cp -a $PKGS_PATH_DIR/zstd*/Library/bin/. ./bin/
-cp $PKGS_PATH_DIR/libtiff*/Library/bin/tiff.dll ./bin/
-cp $PKGS_PATH_DIR/libtiff*/Library/bin/libtiff.dll ./bin/
-cp $PKGS_PATH_DIR/libssh2*/Library/bin/libssh2.dll ./bin/
-cp $PKGS_PATH_DIR/libpng*/Library/bin/libpng16.dll ./bin/
-cp $PKGS_PATH_DIR/libcurl*/Library/bin/libcurl.dll ./bin/
-cp $PKGS_PATH_DIR/openssl*/Library/bin/libcrypto-1_1-x64.dll ./bin/
-cp $PKGS_PATH_DIR/openjpeg*/Library/bin/openjp2.dll ./bin/
-cp $PKGS_PATH_DIR/xz*/Library/bin/liblzma.dll ./bin/
-cp $PKGS_PATH_DIR/cairo*/Library/bin/cairo.dll ./bin/
+cp -a $PKGS_PATH_DIR/poppler-$POPPLER_VERSION*/Library/ .
+cp $PKGS_PATH_DIR/freetype*/Library/bin/freetype.dll ./Library/bin/
+cp $PKGS_PATH_DIR/zlib*/Library/bin/zlib.dll ./Library/bin/
+cp -a $PKGS_PATH_DIR/zstd*/Library/bin/. ./Library/bin/
+cp $PKGS_PATH_DIR/libtiff*/Library/bin/tiff.dll ./Library/bin/
+cp $PKGS_PATH_DIR/libtiff*/Library/bin/libtiff.dll ./Library/bin/
+cp $PKGS_PATH_DIR/libssh2*/Library/bin/libssh2.dll ./Library/bin/
+cp $PKGS_PATH_DIR/libpng*/Library/bin/libpng16.dll ./Library/bin/
+cp $PKGS_PATH_DIR/libcurl*/Library/bin/libcurl.dll ./Library/bin/
+cp $PKGS_PATH_DIR/openssl*/Library/bin/libcrypto-1_1-x64.dll ./Library/bin/
+cp $PKGS_PATH_DIR/openjpeg*/Library/bin/openjp2.dll ./Library/bin/
+cp $PKGS_PATH_DIR/xz*/Library/bin/liblzma.dll ./Library/bin/
+cp $PKGS_PATH_DIR/cairo*/Library/bin/cairo.dll ./Library/bin/
 
-cd ./bin/
-mkdir share && cd share
-mkdir poppler && cd poppler
+mkdir share
+cd share
 curl $POPPLER_DATA_URL --output poppler-data.tar.gz
 tar xvzf poppler-data.tar.gz -C poppler --strip-components 1
 rm poppler-data.tar.gz
