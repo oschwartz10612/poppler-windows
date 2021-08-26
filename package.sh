3dfe04c7ef0047d9c9d94a73f3c5aaa8b7989523
oschwartz10612 <oschwartz10612@gmail.com>
2021-08-25 20:58:21 -0400

    Catch failed command and exit 1

--- package.sh
+++ package.sh
@@ -1,6 +1,9 @@
 POPPLER_VERSION=21.08.0
 POPPLER_DATA_URL="https://poppler.freedesktop.org/poppler-data-0.4.10.tar.gz"
 
+set -e 
+set -o pipefail
+
 mkdir "poppler-$POPPLER_VERSION"
 cd "poppler-$POPPLER_VERSION" || exit
 
